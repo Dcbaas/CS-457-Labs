79f516d4fc67760768f4de465916a69f97f6aa04
Dcbaas <Dcbaas@users.noreply.github.com>
2019-03-15 12:49:46 -0400

    finished

--- lab9/file_utils.c
+++ lab9/file_utils.c
@@ -18,17 +18,7 @@ int read_file( char* filename, char **buffer ){
 
         *buffer = (char*) malloc(size * sizeof(char));
 
-        char ch = 0;
-        int index = 0;
-
-        ch = fgetc(input_file);
-        while(ch != EOF){
-            *(*buffer + index) = ch;
-            ch = fgetc(input_file);
-            ++index;
-
-        }
-
+        fread(*buffer, sizeof(char), size, input_file);
 
         fclose(input_file);
     }
@@ -41,19 +31,18 @@ int read_file( char* filename, char **buffer ){
 }
 
 int write_file( char* filename, char *buffer, int size){
-    FILE* output_file = fopen(filename, "w");
+    FILE* output_file = fopen(filename + 1, "w");
     int status = 1;
 
-
+    printf("%s\n", filename);
     //Ensure that the out file didn't fail.
     if(output_file){
-        //Write the file
-        for(int index = 0; index < size; ++index){
-            fputc(buffer[index], output_file);
-        }
+        fwrite(buffer, sizeof(char), size, output_file);
     }
     else{
+        perror("File failed: ");
         status = -1;
+        return -1;
     }
 
     fclose(output_file);
--- lab9/tcpserver.c
+++ lab9/tcpserver.c
@@ -1,31 +1,48 @@
+#include "file_utils.h"
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/select.h>
 
 int main(int argc, char **argv){
+    int portNum;
+    if(argc != 2){
+        printf("Usage: ./server <port number>\n");
+        return 1;
+    }
+    else{
+        portNum = atoi(argv[1]);
+    }
+    char* sendBuffer;
+    char filename[500];
+    int fileSize;
+    char sizeSender[4];
+
     int sockfd = socket(AF_INET,SOCK_STREAM,0);
     fd_set sockets;
+    FD_ZERO(&sockets);
     FD_SET(sockfd,&sockets);
+    
 
     struct sockaddr_in serveraddr,clientaddr;
     serveraddr.sin_family=AF_INET;
-    serveraddr.sin_port=htons(9876);
+    serveraddr.sin_port=htons(portNum);
     serveraddr.sin_addr.s_addr=INADDR_ANY;
 
     bind(sockfd,(struct sockaddr*)&serveraddr,
             sizeof(serveraddr));
     listen(sockfd,10);
-    printf("pre loop\n");
     while(1){
-        printf("Start of loop");
+        printf("while loop start\n");
         fd_set tmpset=sockets;
         int n = select(FD_SETSIZE,&tmpset,NULL,NULL,NULL);
-        printf("The value of n is %d\n", n);
         if(FD_ISSET(sockfd,&tmpset)){
+            printf("FD_ISSET was true\n");
             int len = sizeof(clientaddr);
             int clientsocket = accept(sockfd,
                     (struct sockaddr*)&clientaddr,&len);
@@ -35,13 +52,29 @@ int main(int argc, char **argv){
             int ii;
             for(ii=0; ii<FD_SETSIZE; ii++){
                 if(FD_ISSET(ii,&tmpset)){
-                    char line[5000];
-                    int n = recv(ii,line,5000,0);
+                    //Recive the filename
+                    int filenameLen = recv(ii,filename,5000,0);
 
-                    printf("Got from client: %s\n",line);
+                    //Find the file
+                    fileSize = read_file(filename, &sendBuffer);
+                    memcpy(sizeSender, &fileSize, sizeof(int));
+
+                    //Send the result
+                    send(ii, sizeSender, 4, 0);
+
+                    //Did the file exist? If it did, send it.
+                    if(fileSize > -1){
+                        send(ii, sendBuffer, fileSize, 0);
+                        printf("The file %s was sent\n", filename);
+                    } 
+                    else{ 
+                        printf("The file %s didn't exist and wasn't sent\n", filename);
+                    }
+
                     FD_CLR(ii,&sockets);
                 }
             }
+            free(sendBuffer);
         }
 
     }
